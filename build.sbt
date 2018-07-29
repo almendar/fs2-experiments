0f6c7d4a15e2fc52f776cd0a907cec0f214b0282
Tomasz Kogut <t.kogut@Tomaszs-MacBook-Pro.local>
2018-07-29 23:19:02 +0200

    Updated resource managmnet section

--- build.sbt
+++ build.sbt
@@ -4,5 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
-libraryDependencies += "co.fs2" %% "fs2-core" % "0.10.5"
+libraryDependencies ++= Seq(
+  "co.fs2" %% "fs2-core" % "0.10.5",
+  "co.fs2" %% "fs2-io" % "0.10.5"
+)
 scalafmtOnCompile := true
--- src/main/scala/ResourceManagment.scala
+++ src/main/scala/ResourceManagment.scala
@@ -1,6 +1,6 @@
 import cats.effect.IO
 import fs2.StreamApp.ExitCode
-import fs2.{Scheduler, Stream, StreamApp}
+import fs2.{Chunk, Scheduler, Stream, StreamApp}
 
 trait PrintMe extends AutoCloseable { self =>
   println(s"Opening ${self.getClass}")
@@ -25,21 +25,35 @@ class Resource2 extends PrintMe {
 class Resource3 extends PrintMe {
   type Res2 = Resource3
   override def create(): Resource3 = this
+  def unConst(): String            = { scala.util.Random.alphanumeric.take(5).mkString }
 }
 
 object ResourceManagment extends StreamApp[IO] {
+
+  val printer: Stream[IO, String] = for {
+    res2 <- Stream.bracket(IO(new Resource1))({ r1 =>
+      Stream.eval(IO(r1.open))
+    }, { r1 =>
+      IO(r1.close())
+    })
+    res3 <- Stream.bracket(IO.pure(res2))({ r2 =>
+      Stream.eval(IO(r2.open))
+    }, { r2 =>
+      IO(r2.close())
+    })
+    character <- Stream.bracket(IO.pure(res3))({ r3 =>
+      Stream.eval(IO(r3.open.unConst)).repeat
+    }, { r3 =>
+      IO(r3.close())
+    })
+  } yield character
+
   override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, ExitCode] =
-    for {
-      res2 <- Stream.bracket(IO(new Resource1))(use = { r1 =>
-        Stream.eval(IO(r1.open))
-      }, release = { r1 =>
-        IO(r1.close())
-      })
-      res3 <- Stream.bracket(IO.pure(res2))(use = { r2 =>
-        Stream.eval(IO(r2.open))
-      }, release = { r2 =>
-        IO(r2.close())
-      })
-    } yield ExitCode.Success
+    (printer
+      .flatMap(x => Stream.chunk(Chunk.bytes((x + "\n").getBytes))))
+      .through(fs2.io.stdout)
+      .take(10)
+      .drain
+      .covaryOutput[ExitCode] ++ Stream.emit(ExitCode.Success)
 
 }
